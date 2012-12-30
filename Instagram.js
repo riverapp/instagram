73d29ef408e5e7ba2bd502ddf765d46e70bd5111
Dan Palmer <dan.palmer@me.com>
2012-12-30 23:53:54 +0000

    Initial commit

---
+++ Instagram.js
@@ -0,0 +1,51 @@
+(function() {
+
+	var Instagram = function(delegate) {
+		this.OAUTH_CLIENT_ID = '34498eab82ad46b481c1b394cd9e879a';
+		console.log(delegate.callbackURL());
+		this.oauth_authorize_token_url =
+			'https://api.instagram.com/oauth/authorize/?client_id=' + this.OAUTH_CLIENT_ID +
+			'&redirect_uri=' + encodeURIComponent(delegate.callbackURL()) +
+			'&response_type=token&scope=basic';
+
+		this.delegate = delegate;
+	};
+
+	Instagram.prototype.authRequirements = function(callback) {
+		callback({
+			authType: "oauth",
+			url: this.oauth_authorize_token_url
+		});
+	};
+
+	Instagram.prototype.authenticate = function(params) {
+		var self = this;
+		HTTP.request({
+			method: 'GET',
+			url: 'https://api.instagram.com/v1/users/self/?access_token=' + params.access_token
+		}, function(err, response) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+			response = JSON.parse(response);
+			self.delegate.createAccount({
+				name: response.data.username,
+				identifier: response.data.id,
+				secret: params.access_token,
+				avatarURL: response.data.profile_picture
+			});
+		});
+	};
+
+	Instagram.prototype.updatePreferences = function(callback) {
+		callback({
+			'interval': 180,
+			'min': 60,
+			'max': 900
+		});
+	};
+
+	PluginManager.registerPlugin(Instagram, 'me.danpalmer.River.plugins.Instagram');
+
+})();
